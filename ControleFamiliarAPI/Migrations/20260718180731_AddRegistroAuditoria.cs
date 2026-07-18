9bbdc323cc48cd0faf1b06f797fda91d03e4a28a
gentilpedro <gentil.pedro21@gmail.com>
2026-07-18 15:07:54 -0300

    Adiciona migration AddRegistroAuditoria

---
+++ ControleFamiliarAPI/Migrations/20260718180731_AddRegistroAuditoria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleFamiliarAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRegistroAuditoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RegistrosAuditoria",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    FamiliaId = table.Column<int>(type: "int", nullable: false),
+                    UsuarioAlvoId = table.Column<int>(type: "int", nullable: true),
+                    Acao = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegistrosAuditoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RegistrosAuditoria_FamiliaId_CriadoEm",
+                table: "RegistrosAuditoria",
+                columns: new[] { "FamiliaId", "CriadoEm" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RegistrosAuditoria");
+        }
+    }
+}
